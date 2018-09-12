c8f1e01e075b1ac20ee1f2e72853bdb007ac9aa2
jmubago <jmubagosotomayor@gmail.com>
2018-09-12 09:57:45 +0200

    Agregar archivos de proyecto.

---
+++ 02_ReviewExtension/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _02_ReviewExtension
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstView = new System.Windows.Forms.ListView();
+            this.BtnExercise1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LstView
+            // 
+            this.LstView.Location = new System.Drawing.Point(295, 38);
+            this.LstView.Name = "LstView";
+            this.LstView.Size = new System.Drawing.Size(292, 268);
+            this.LstView.TabIndex = 0;
+            this.LstView.UseCompatibleStateImageBehavior = false;
+            // 
+            // BtnExercise1
+            // 
+            this.BtnExercise1.Location = new System.Drawing.Point(16, 40);
+            this.BtnExercise1.Name = "BtnExercise1";
+            this.BtnExercise1.Size = new System.Drawing.Size(209, 45);
+            this.BtnExercise1.TabIndex = 1;
+            this.BtnExercise1.Text = "Exercise 1";
+            this.BtnExercise1.UseVisualStyleBackColor = true;
+            this.BtnExercise1.Click += new System.EventHandler(this.BtnExercise1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 380);
+            this.Controls.Add(this.BtnExercise1);
+            this.Controls.Add(this.LstView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView LstView;
+        private System.Windows.Forms.Button BtnExercise1;
+    }
+}
+
---
+++ 02_ReviewExtension/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_ReviewExtension
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //***************** Exercise 1 *****************//
+        //Write a program which will simulate the tossing of 4 fair coins, 50 times. 
+        //Display the results of each toss of four on a
+        //separate line.If exactly 3 heads occur on a give toss, 
+        //follow the line with an asterisk (*). 
+
+        private void BtnExercise1_Click(object sender, EventArgs e)
+        {
+            LstView.Clear();
+            int dice = 0;
+            string sumOfDice = "";
+            int countThreeH = 0;
+
+            RollDice(dice, sumOfDice, countThreeH);
+        }
+
+        private void RollDice(int dice, string sumOfDice, int countThreeH)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 50; i++)
+            {
+                int countH = 0;
+                sumOfDice = "";
+                for (int x = 0; x < 4; x++)
+                {
+                    dice = r.Next(1, 3);
+                    if (dice == 1)
+                    {
+                        sumOfDice += "H";
+                        countH++;
+                    }
+                    else
+                    {
+                        sumOfDice += "T";
+                    }
+                    
+                }
+                if (countH == 3)
+                {
+                    countThreeH++;
+                    sumOfDice += "*";
+                }
+
+                LstView.Items.Add(sumOfDice);
+            }
+            LstView.Items.Add("there were " + countThreeH + " toss with 3 H");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
